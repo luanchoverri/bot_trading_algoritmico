a711a74147b0419978d7d0c1dfc699e749aee759
luanchoverri <lu.anchoverri@gmail.com>
2023-12-01 03:23:51 -0300

    cross fixed

--- goldencross_strategy.py
+++ goldencross_strategy.py
@@ -5,43 +5,41 @@ class CrossStrategy(bt.Strategy):
     params = (
         ('position_size', 0.1),
         ('atr_period', 14),      # Periodo para el cálculo del ATR
-        ('atr_multiplier_sl', 0.8),  # Multiplicador para el stop loss basado en ATR
-        ('atr_multiplier_tp', 1.5),  # Multiplicador para el take profit basado en ATR
+        ('atr_multiplier_sl', 1),  # Multiplicador para el stop loss basado en ATR
+        ('atr_multiplier_tp', 2),  # Multiplicador para el take profit basado en ATR
         ('rsi_period', 14),
-        ('rsi_upper', 60),
-        ('rsi_lower', 40),
+        ('rsi_upper', 65),
+        ('rsi_lower', 35),
         ('sma_fast', 50),        # Periodo para la media móvil rápida
         ('sma_slow', 200)        # Periodo para la media móvil lenta
     )
 
     def log(self, txt, dt=None):
-        ''' Logging function '''
+        ''' Función de registro '''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
-        # Moving averages
+        # Medias móviles
         self.sma_fast = btind.SimpleMovingAverage(period=self.params.sma_fast)
         self.sma_slow = btind.SimpleMovingAverage(period=self.params.sma_slow)
 
         # RSI
         self.rsi = btind.RSI(period=self.params.rsi_period)
 
-        # Cross over indicators
+        # Cruces de medias móviles
         self.golden_cross = btind.CrossOver(self.sma_fast, self.sma_slow)
         self.death_cross = btind.CrossOver(self.sma_slow, self.sma_fast)
 
         # ATR
         self.atr = btind.ATR(period=self.params.atr_period)
 
-        # Tracking for dynamic SL and TP
+        # Seguimiento para SL y TP dinámicos
         self.sl_price = None
         self.tp_price = None
 
-
-
     def next(self):
-        # Calculating dynamic SL and TP based on ATR
+        # Calculando SL y TP dinámicos basados en ATR
         atr_value = self.atr[0]
         current_price = self.data.close[0]
         self.sl_price = current_price - atr_value * self.params.atr_multiplier_sl
@@ -49,23 +47,17 @@ class CrossStrategy(bt.Strategy):
 
         if not self.position:
             if self.golden_cross > 0 and self.rsi < self.params.rsi_upper:
-                # Buy signal - Golden Cross confirmed with RSI
+                # Señal de compra - Golden Cross confirmado con RSI
                 size = self.broker.getvalue() * self.params.position_size / self.data.close
-                self.buy(size=size)
-                self.log('BUY EXECUTED, Price: %.2f, Size: %.2f' % (current_price, size))
+                self.buy(size=size, exectype=bt.Order.Stop, price=self.tp_price)
+                self.log('COMPRA, Precio: %.2f, Tamaño: %.2f, TP: %.2f' % (current_price, size, self.tp_price))
+
+            elif self.death_cross < 0 and self.rsi > self.params.rsi_lower:
+                # Señal de venta - Death Cross confirmado con RSI
+                self.sell(exectype=bt.Order.Stop, price=self.sl_price)
+                self.log('VENTA, Precio: %.2f, SL: %.2f' % (current_price, self.sl_price))
 
         elif self.death_cross < 0 and self.rsi > self.params.rsi_lower:
-            # Sell signal - Death Cross confirmed with RSI
+            # Señal de venta - Death Cross confirmado con RSI
             self.close()
-            self.log('SELL EXECUTED, Price: %.2f' % current_price)
-
-        # Check for SL and TP conditions
-        if self.position:
-            if current_price <= self.sl_price:
-                # Stop Loss hit
-                self.close()
-                self.log('STOP LOSS HIT, Price: %.2f' % current_price)
-            elif current_price >= self.tp_price:
-                # Take Profit hit
-                self.close()
-                self.log('TAKE PROFIT HIT, Price: %.2f' % current_price)
+            self.log('VENTA EJECUTADA, Precio: %.2f' % current_price)
--- sma_strategy.py
+++ sma_strategy.py
@@ -1,86 +1,43 @@
 import backtrader as bt
 
 class SMAStrategy(bt.Strategy):
-    params = (
-        ('maperiod', 20),
-    )
-
-    def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
-
+    # al inicializar la instancia, se crea un indicador de Media Móvil Simple (MA) de 250 días utilizando el precio de cierre
     def __init__(self):
-
-        self.dataclose = self.datas[0].close
-
+        self.ma = bt.indicators.SimpleMovingAverage(self.data.close, period=250)
         self.order = None
-        self.buyprice = None
-        self.buycomm = None
-
-
-        self.sma = bt.indicators.SimpleMovingAverage( self.datas[0], period=self.params.maperiod)
-
-        # indicadores plot
-        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
-        bt.indicators.WeightedMovingAverage(self.datas[0], period=25,
-                                            subplot=True)
-        bt.indicators.StochasticSlow(self.datas[0])
-        bt.indicators.MACDHisto(self.datas[0])
-        rsi = bt.indicators.RSI(self.datas[0])
-        bt.indicators.SmoothedMovingAverage(rsi, period=10)
-        bt.indicators.ATR(self.datas[0], plot=False)
-
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            return
-
-
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log(
-                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                    (order.executed.price,
-                     order.executed.value,
-                     order.executed.comm))
-
-                self.buyprice = order.executed.price
-                self.buycomm = order.executed.comm
-            else:
-                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                         (order.executed.price,
-                          order.executed.value,
-                          order.executed.comm))
-
-            self.bar_executed = len(self)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
-
-        self.order = None
-
-    def notify_trade(self, trade):
-        if not trade.isclosed:
-            return
-
-        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
-                 (trade.pnl, trade.pnlcomm))
 
     def next(self):
-
-        self.log('Close, %.2f' % self.dataclose[0])
-
         if self.order:
             return
-
-        if not self.position:
-
-            if self.dataclose[0] > self.sma[0]:
-                self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                self.order = self.buy()
-
+        if not self.position:  # verifica si ya tienes una posición en el mercado
+            if (self.data.close[0] > self.ma[0]) & (self.data.close[-1] < self.ma[-1]):
+                self.log('Crear Orden de Compra, %.2f' % self.data.close[0])
+                self.order = self.buy(size=10)  # comprar cuando el precio de cierre hoy cruza por encima de la MA.
+            if (self.data.close[0] < self.ma[0]) & (self.data.close[-1] > self.ma[-1]):
+                self.log('Crear Orden de Venta, %.2f' % self.data.close[0])
+                self.order = self.sell(size=10)  # vender cuando el precio de cierre hoy esté por debajo de la MA
         else:
+            # Esto significa que tienes una posición, por lo tanto, necesitas definir la estrategia de salida aquí.
+            if len(self) >= (self.bar_executed + 4):
+                self.log('Posición Cerrada, %.2f' % self.data.close[0])
+                self.order = self.close()
 
-            if self.dataclose[0] < self.sma[0]:
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
-                self.order = self.sell()
+    # muestra información
+    def log(self, txt):
+        dt = self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def notify_order(self, order):
+        if order.status == order.Completed:
+            if order.isbuy():
+                self.log(
+                    "COMPRA (Precio: %.2f, Valor: %.2f" %
+                    (order.executed.price, order.executed.value, order.executed.comm))
+            else:
+                self.log(
+                    "VENTA (Precio: %.2f, Valor: %.2f)" %
+                    (order.executed.price, order.executed.value))
+            self.bar_executed = len(self)
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log("La orden fue cancelada/margen/rechazada")
+        self.order = None
